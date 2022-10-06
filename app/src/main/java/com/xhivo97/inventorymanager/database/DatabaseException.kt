88301ab6bcac8b498bb7dab13d024f9a7d51a79e
Xhivat Hoxhiq <xhivo97@gmail.com>
2022-10-06 12:41:39 +0200

    Style fix formatting

--- app/src/main/java/com/xhivo97/inventorymanager/database/DatabaseException.kt
+++ app/src/main/java/com/xhivo97/inventorymanager/database/DatabaseException.kt
@@ -2,14 +2,14 @@ package com.xhivo97.inventorymanager.database
 
 class DBProductAlreadyExists(
     message: String? = null,
-    cause: Throwable? = null
+    cause: Throwable? = null,
 ) : Exception(message, cause) {
     constructor(cause: Throwable) : this(null, cause)
 }
 
 class DBProductDoesNotExist(
     message: String? = null,
-    cause: Throwable? = null
+    cause: Throwable? = null,
 ) : Exception(message, cause) {
     constructor(cause: Throwable) : this(null, cause)
 }
--- app/src/main/java/com/xhivo97/inventorymanager/ui/theme/Theme.kt
+++ app/src/main/java/com/xhivo97/inventorymanager/ui/theme/Theme.kt
@@ -3,7 +3,12 @@ package com.xhivo97.inventorymanager.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -41,7 +46,7 @@ fun InventoryManagerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
